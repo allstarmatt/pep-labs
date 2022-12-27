16df12ecb88d0bde8599239984e3f32ce59ec537
allstarmatt <august.posey@gmail.com>
2022-12-27 18:23:40 +0000

    Saved progress on Instantiation 2022-12-27 18:23:40 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,8 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+        Kingdom king = new Kingdom();
+        return king;
     }
 }
