d698b460e25daf4e4d4f040b47408f8288c25dbb
allstarmatt <august.posey@gmail.com>
2022-12-31 19:32:59 +0000

    Saved progress on ReturnArrayContents 2022-12-31 19:32:59 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -27,17 +27,14 @@ public class ReturnArrayContents {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-        public static void main(String[] args) {
-            
-        }
-
     public String arrayToString(int[] nums){
-        String str;
+        String str = "";
         
         for (int i = 0; i < nums.length; i++) {  
-            str = " ";
-            str = str + nums[i];
-
+            str = str + nums[i]+ " ";
+            
+            
+            
         }
         
         return str;
