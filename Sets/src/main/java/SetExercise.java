c966484f0d1f4c72b7199285d9c86c6db8594d81
allstarmatt <august.posey@gmail.com>
2023-01-05 14:26:28 +0000

    Saved progress on Lists 2023-01-05 14:26:28 +0000

--- Sets/src/main/java/SetExercise.java
+++ Sets/src/main/java/SetExercise.java
@@ -20,6 +20,7 @@ public class SetExercise {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
+        <Integer> hash = new Set<Integer>();
         return null;
     }
 
