ae512f7e59aa41ab74769b15bc0ed814e6c9a7e6
allstarmatt <august.posey@gmail.com>
2023-01-06 01:01:30 +0000

    Saved progress on Sets 2023-01-06 01:01:30 +0000

--- Sets/src/main/java/SetExercise.java
+++ Sets/src/main/java/SetExercise.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -20,8 +21,8 @@ public class SetExercise {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        <Integer> hash = new Set<Integer>();
-        return null;
+        HashSet<Integer> hash = new HashSet<Integer>();
+        return hash;
     }
 
     /**
@@ -30,7 +31,7 @@ public class SetExercise {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -40,6 +41,7 @@ public class SetExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
+        set.add(item);
 
     }
 
@@ -50,6 +52,6 @@ public class SetExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
